030c69d86511170a6e3a37982579c730645b1dfb
Kr0sHkA <pisleginkrut@gmail.com>
2023-12-13 07:37:50 +0700

    laba6
    laba6

---
+++ laba6n1/laba6n1/laba6n1.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <clocale>
+
+/*
+ *  Л/р 6 Двумерные массивы
+ * Пислегин Евгений Константинович ИВТ - 32
+ * Дана матрица размера M × N. 
+ * Вывести ее элементы в следующем порядке:
+ * первый столбец сверху вниз,
+ * второй столбец снизу вверх, 
+ * третий столбец сверху вниз,
+ * четвертый столбец снизу вверх и т. д.
+*/
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int m;
+    int n;
+    cout << "Введите количество строк массива: ";
+    cin >> m;
+    cout << "Введите количество столбцов массива: ";
+    cin >> n;
+
+    int** a = new int* [m];
+    for (int i = 0; i < m; i++) {
+        a[i] = new int[n];
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            a[i][j] = rand() % 10 - 1;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << a[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (j % 2 == 0) {
+                cout << a[i][j] << "\t";
+            }
+            else {
+                cout << a[m - i - 1][j] << "\t";
+            }
+        }
+        cout << ("\n");
+    }
+
+    for (int i = 0; i < m; i++) {
+        delete[] a[i];
+    }
+    delete[] a;
+}
+
---
+++ laba6n2/laba6n2/laba6n2.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <clocale>
+/*
+ *  Л/р 6 Двумерные массивы
+ * Пислегин Евгений Константинович ИВТ - 32
+ * Дана матрица размера M × N и целое число K (1 ≤ K ≤ N ).
+ * Удалить столбец матрицы с номером K
+*/
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int m;
+    int n;
+    int k;
+    cout << "Введите количество строк массива: ";
+    cin >> m;
+    cout << "Введите количество столбцов массива: ";
+    cin >> n;
+    cout << "Введите номер столбца: ";
+    cin >> k;
+
+    int** a = new int* [m];
+    for (int i = 0; i < m; i++) {
+        a[i] = new int[n];
+    }
+    
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            a[i][j] = rand() % 10 - 1;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << a[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++) {
+            if ((k-1) == j) {
+                    for (int strok = 0; strok < m; strok++) {
+                        for (int stolb = j; stolb < n; stolb++) {
+                            a[strok][stolb] = a[strok][stolb + 1];
+                        }
+                    }
+                    n--;
+                    goto next;
+            }
+        }
+    }
+next:
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << a[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < m; i++) {
+        delete[] a[i];
+
+    }
+    delete[] a;
+}
